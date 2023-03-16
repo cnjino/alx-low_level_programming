1d9585e327a27dbe467e5059cf8542c119237aff
cnjino <charlesnjino@gmail.com>
2023-03-16 13:21:13 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -prints "A program that prints a line to the standard error
+ * Return: Always (1)
+ */
+
+int main(void)
+
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59)
+	return (1)
+}
