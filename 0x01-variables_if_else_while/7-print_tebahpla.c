a80337e6a99a92e0ee1d3b179bfae77e62e33642
cnjino <charlesnjino@gmail.com>
2023-03-17 13:28:10 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * mani - Prints all single digit number of base 10 starting from 0
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
