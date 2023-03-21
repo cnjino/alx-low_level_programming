06e589f63cf0003fe514a46bdda5bcd44cb3db7a
cnjino <charlesnjino@gmail.com>
2023-03-21 18:30:47 +0000

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,15 +9,15 @@
 
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n >= 99)
 	{
-		while (n > 98)
+		while (n > 99)
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 	else
 	{
-		while (n < 98)
+		while (n < 99)
 			printf("%d, ", n++);
 	}
 }
