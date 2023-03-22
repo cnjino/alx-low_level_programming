53e5572075e565261a7d407d4bfa55d45a7a572d
cnjino <charlesnjino@gmail.com>
2023-03-22 10:31:12 +0000

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,10 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	/* Insert the value of i++ */
-	while (i < 10)
+	/**while (i < 10)
 	{
 	putchar(i);
-	}
+	} */
 
 	printf("Infinite loop avoided! \\o/\n");
 
