ba70f35fdd39d24f52bcb1527988dca9b82c27ce
cnjino <charlesnjino@gmail.com>
2023-03-23 15:31:53 +0000

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,16 +10,19 @@ void print_square(int size)
 {
 	int x, y;
 
-	for (size >= 0)
+	for (size <= 0)
 	{
 	_putchar('\n');
 	}
 	else
 	{
-	for (x = 0; x > size; x++)
+	for (x = 0; x < size; x++)
 	{
-	for (y = 0; y > size; y++)
-	}
+	for (y = 0; y < size; y++)
+	{
 	_putchar(35);
+	}
+	_putchar('\n');
+	}
 	}
 }
