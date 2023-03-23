996c4db2d402e6374622b24a967361a462f6001c
cnjino <charlesnjino@gmail.com>
2023-03-23 15:22:08 +0000

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: number of squares
+ * Return: no return
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	for (size >= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x > size; x++)
+	{
+	for (y = 0; y > size; y++)
+	}
+	_putchar(35);
+	}
+}
