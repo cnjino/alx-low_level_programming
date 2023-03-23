c701841a74f17367696f376cdf6c06a0aa18b92a
cnjino <charlesnjino@gmail.com>
2023-03-23 10:50:30 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints number from 0 to 9
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	if (c >= 0 && c <= 9)
+	{
+		_putchar(c);
+		c++;
+	}
+	{
+		_putchar('\n');
+	}
+	return (0);
+}
+
