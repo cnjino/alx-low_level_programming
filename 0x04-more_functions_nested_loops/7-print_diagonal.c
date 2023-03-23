bc311820b171ecf1536e891781ebb2fea310786c
cnjino <charlesnjino@gmail.com>
2023-03-23 14:16:17 +0000

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: the number of lines to draw
+ * Return: no return
+ */
+
+void print_line(int n)
+{
+
+	int c;
+	
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (c = 0; c < n; c++)
+	{
+	_putchar('/');
+	}
+	_putchar('\n');
+									        }
+}
