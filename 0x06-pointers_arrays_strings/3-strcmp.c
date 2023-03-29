d0d4c84721ceab8293372652545b925643ac5d55
cnjino <charlesnjino@gmail.com>
2023-03-29 17:57:39 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * _strcmp - compare two strings
  * @s1: string 1
  * @s2: string 2
- * Return: s1[i] and s2[i].
+ * Return: the difference in s1[i] and s2[i].
  */
 
 int _strcmp(char *s1, char *s2)
@@ -15,10 +15,10 @@ int _strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-	if (s1[i] != s2[i]);
-	{
-	return (s1[i] - s2[i]);
-	}
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	i++;
 	}
 	return (0);
