f9dfc3d51165e3b6322dfbcc63bbae60a463f770
cnjino <charlesnjino@gmail.com>
2023-03-29 10:29:18 +0000

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase
+ * @n: pointer
+ * Return: Always 0.
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - ' ';
+		i++;
+	}
+	return (0);
+}
