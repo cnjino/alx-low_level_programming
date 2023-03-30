3be54c46236f1bb187f122ca90f53638f436f1f3
cnjino <charlesnjino@gmail.com>
2023-03-30 09:57:12 +0000

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,15 +11,15 @@ void print_number(int n)
 {
 	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		x = -n;
 	}
-	x = n;
-	if (x / 10)
+	if (x / 10 != '0')
 	{
 		print_number(x / 10);
-	_putchar(x % 10 + '0');
 	}
+	_putchar(x % 10 + '0');
 }
