fbf66a8bd255f0aa01bda21a66a51edf78143d07
cnjino <charlesnjino@gmail.com>
2023-03-31 02:15:23 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,13 +15,13 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
-		n1 = -n;
+	putchar('45');
+	n1 = -n;
 	}
 
 	if (n1 / 10 != 0)
 	{
-		print_number(n1 / 10);
+	print_number(n1 / 10);
 	}
 	putchar((n1 % 10) + '0');
 }
