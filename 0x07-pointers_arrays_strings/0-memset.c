c6f6203962b93f6d7571ee4100d2734497463d57
cnjino <charlesnjino@gmail.com>
2023-04-03 03:18:23 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-        char *ptr = s;
+	char *ptr = s;
 
-        while (n-- > 0)
-        {
-                *ptr++ = b;
-        }
-        return (s);
+	while (n-- > 0)
+	{
+		*ptr++ = b;
+	}
+	return (s);
 }
