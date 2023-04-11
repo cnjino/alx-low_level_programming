f26e6242a39d3601a253489ff4178cd660389066
cnjino <charlesnjino@gmail.com>
2023-04-11 08:00:01 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,32 +11,34 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
+	int i, j, k =0, len = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 			len++;
 		len++;
 	}
+	len += ac;
 	str = malloc(sizeof(char) * (len + 1));
 
 	if (str == NULL)
 		return (NULL);
-	k = 0;
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 		{
 			str[k] = av[i][j];
 			k++;
 		}
-		str[k] = '\n';
-		k++;
+		if (str[k] == '\0');
+		{
+			str[k++] = '\n';
+		}
 	}
 	return (str);
 }
