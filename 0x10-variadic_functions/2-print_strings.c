f54453053fcd29515a8676d5fd4a1790ad3eff2d
cnjino <charlesnjino@gmail.com>
2023-04-20 07:14:47 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	unsigned int index;
 
-	va_start(string, n);
+	va_start(strings, n);
 
 	for (index = 0; index < n; index++)
 	{
