f576328e654cbfc91bc89c387d045b81fbfbcefb
cnjino <charlesnjino@gmail.com>
2023-04-27 05:07:36 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,10 +20,13 @@ list_t *add_node_end(list_t **head, const char *str)
 	len++;
 
 	new = malloc(sizeof(list_t));
-	if (new)
+	if (new == NULL)
 	return (NULL);
 
 	new->str = strdup(str);
+	if (new->str == NULL)
+	free (new);
+	return (NULL);
 	new->len = len;
 	new->next = NULL;
 
