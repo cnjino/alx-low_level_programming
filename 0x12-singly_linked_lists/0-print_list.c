7a6aea33ff1d32af8d832a3bac75f2d17b8caf5d
cnjino <charlesnjino@gmail.com>
2023-04-27 03:54:45 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	int count = 0;
 
 	while (h)
 	{
-	if (!h->str)
-	printf("[0] (nil)\n");
-	else
-	printf("[%u] %s\n", h->len, h->str);
-	h = h->next;
-	count++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
 	}
 
 	return (count);
