ffc74fdb9d7e16898f646b1bfc17161076cf560a
cnjino <charlesnjino@gmail.com>
2023-05-08 04:14:58 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 06600);
-	Y = write(fd, text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	y = write(fd, text_content, len);
 
 	if (fd == -1 || y == -1)
 		return (-1);
